b241c61f4a8a1a1a7dcccdf00e96d8e2aba7f765
mithun_babbira <mithunbabbira@gmail.com>
2019-12-21 15:36:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/facedection/ResultDialog.java
@@ -0,0 +1,97 @@
+//package com.example.facedection;
+//
+//import android.os.Bundle;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.Button;
+//import android.widget.TextView;
+//
+//import androidx.annotation.NonNull;
+//import androidx.annotation.Nullable;
+//import androidx.fragment.app.DialogFragment;
+//
+//
+//
+//
+//
+//
+//public class ResultDialog extends DialogFragment {
+//
+//    private Button okButton;
+//    private TextView resultTV;
+//
+//
+//    @Nullable
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        View view = inflater.inflate(R.layout.fragment_resultdialog, container, false);
+//        String resultText = "";
+//        okButton = view.findViewById(R.id.result_ok_button);
+//        resultTV = view.findViewById(R.id.result_text_view);
+//
+//        Bundle bundle = getArguments();
+//        resultText = bundle.getString(BabbiFaceDection.RESULT_TEXT);
+//
+//        resultTV.setText(resultText);
+//
+//        okButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                dismiss();
+//            }
+//        });
+//
+//
+//
+//        return view;
+//    }
+//}
+
+
+
+package com.example.facedection;
+
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class ResultDialog extends DialogFragment {
+
+    private Button okBUtton;
+    private TextView resultTV;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_resultdialog, container, false);
+        String resultText = "";
+        okBUtton = view.findViewById(R.id.result_ok_button);
+        resultTV = view.findViewById(R.id.result_text_view);
+
+        Bundle bundle = getArguments();
+        resultText = bundle.getString(BabbiFaceDection.RESULT_TEXT);
+
+        resultTV.setText(resultText);
+
+        okBUtton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+
+
+        return view;
+    }
+}
